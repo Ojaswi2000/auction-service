b002485797c55548075277a3609a282079ab43c6
Ojaswi <ojaswiawasthi2025@gmail.com>
2022-11-19 19:33:16 +0530

    created Auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,18 +1,19 @@
-'use strict';
+ async function createAuction(event) {
 
-module.exports.createAuction = async (event) => {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    "statusCode": 200,
+    "body": JSON.stringify(event)
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.handler = createAuction;
