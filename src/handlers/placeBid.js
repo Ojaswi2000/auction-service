0ef7c6e364c34f818c5a0cd922559b64e1e00150
Ojaswi <ojaswiawasthi2025@gmail.com>
2022-11-25 19:37:32 +0530

    updated using patch

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -6,13 +6,29 @@ async function placeBid(req) {
 
   const { id } = req.pathParameters;
   const { amount } = JSON.parse(req.body);
+  const params = {
+    TableName: 'AuctionsTable',
+    Key : {id},
+    UpdateExpression : 'set highestBid.amount = :amount',
+    ExpressionAttributeValues: {
+      ':amount' : amount
+    },
+    ReturnValues : 'ALL_NEW'
+  }
 
-
+  let updatedAuction;
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
 
   return{
     statusCode : 200,
-    body : JSON.stringify(auction)
+    body : JSON.stringify(updatedAuction)
   }
 }
 
